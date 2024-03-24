0fb9ff8b72d8eedb8d48b88f0d838eeb1a44f0bb
logmck <logan.mck4@gmail.com>
2024-03-23 21:41:21 -0600

    Add project files.

---
+++ GuitarShop/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GuitarShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("[action]")]
+        public IActionResult About()
+        {
+            return View();
+        }
+
+
+
+        [Route("[action]")]
+        //New action method for Contact us that
+        public IActionResult ContactUs()
+        {
+            //creates a new dictionary and add the data below to then be displayed on the page
+            Dictionary<string,string> contact = new Dictionary<string, string>
+            {
+                //dictionary data
+                {"Phone", "111-222-3333"},
+                {"Email", "info@info.com" }
+            };
+            //sends the new dictionary through to then be displayed
+            return View(contact);
+        }
+    }
+}
